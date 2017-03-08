fdb2e3acb5c42244fb6144d4cdc3dbef10f9f8ff
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-08 16:51:16 -0500

    slider broke. language checkboxes work now. need to put value of counter into form text

--- src/ui.js
+++ src/ui.js
@@ -4,24 +4,35 @@ window.jobApp = window.jobApp || {};
 
   document.querySelector("#full-name").addEventListener("blur", function checkLength(e) {
       console.log("BLUR TEST");
-      console.log(document.querySelector("#full-name").value);
-      if (document.querySelector("#full-name").value.length === 0) {
-        document.querySelector("#full-name").parentNode.classList.add("has-error");
+      console.log(e.target);
+      if (e.target.value.length === 0) {
+        e.target.parentNode.parentNode.classList.add("has-error");
         console.log("ERROR");
       } else {
+        e.target.parentNode.parentNode.classList.remove("has-error");
         console.log("NO ERROR");
       }
 });
 
 document.querySelector("#exp").addEventListener("change", function updateXp(e) {
   console.log("CHANGE TEST");
-  document.querySelector("figure").innerText = document.querySelector("#exp").value + " years";
+  e.target.innerText = e.target.value + " years";
 
 });
 
+let languageCount = 0;
 
-
-
+Array.from(document.querySelectorAll("[name=languages]")).forEach(function checkStatus(each) {
+  each.addEventListener("change", function countLang(e) {
+    if (e.target.checked === true) {
+      languageCount++;
+      console.log(languageCount);
+    } else {
+      languageCount--;
+      console.log(languageCount);
+    }
+  });
+});
 
 // ********
   // ;

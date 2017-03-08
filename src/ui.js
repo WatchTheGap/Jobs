eb7f182f8ee727c847c23e4b305b98a8c7ad31d0
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-08 14:21:45 -0500

    completed log tests for error handling on full name input field.

--- src/ui.js
+++ src/ui.js
@@ -4,9 +4,9 @@ window.jobApp = window.jobApp || {};
 
   document.querySelector("#full-name").addEventListener("blur", function checkLength(e) {
       console.log("BLUR TEST");
-      if (value.length = 0) {
-        e.classList.add("has-error");
-        console.log("HAS ERROR");
+      console.log(document.querySelector("#full-name").value);
+      if (document.querySelector("#full-name").value.length === 0) {
+        console.log("ERROR");
       } else {
         console.log("NO ERROR");
       }
